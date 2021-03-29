7bd09b76a8fc946c6c409953abe37cd8c06e0b1c
Deividi Zaions <engenharia3@grupowtec.com.br>
2021-03-29 20:52:03 -0300

    Atualização de dados

--- uart-android-test/app/src/main/java/com/example/uart_android_test/MainActivity.kt
+++ uart-android-test/app/src/main/java/com/example/uart_android_test/MainActivity.kt
@@ -18,16 +18,21 @@ class MainActivity : AppCompatActivity() {
     lateinit var connection: UsbDeviceConnection
     lateinit var port: UsbSerialPort
 
+    private val WRITE_WAIT_MILLIS = 200
+    private val READ_WAIT_MILLIS = 200
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-
         setContentView(binding.root)
 
         var botaoConectar = binding.btConectar
         var botaoDesconectar = binding.btDesconectar
         var botaoLigarLed = binding.btLigarLed
         var botaoDesligarLed = binding.btDesligarLed
+        var botaoColetarDados = binding.btColetarDados
+        var botaoColetarDadosCalibrados = binding.btColetarDadosCalibrados
+        var botaoColetarRetorno = binding.btColetarRetorno
 
         botaoConectar.setOnClickListener{
 
@@ -56,24 +61,94 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        botaoLigarLed.setOnClickListener{
-            val ligaLed = byteArrayOf(0x41,0x54,0x4C,0x45,0x44,0x30,0x3D,0x31,0x0D,0x0A)
-
-            port.write(ligaLed, 0)
-
-            Toast.makeText(applicationContext,ligaLed.toString(),Toast.LENGTH_SHORT).show()
-
-        }
-
-        botaoDesligarLed.setOnClickListener{
-            val desligaLed = byteArrayOf(0x41,0x54,0x4C,0x45,0x44,0x30,0x3D,0x30,0x0D,0x0A)
-
-            port.write(desligaLed, 0)
-            Toast.makeText(applicationContext,"ATLEDC=0".toByteArray().toString(),Toast.LENGTH_SHORT).show()
-        }
         botaoDesconectar.setOnClickListener{
             port.close()
         }
 
+        botaoLigarLed.setOnClickListener{
+            val ligaLed = byteArrayOf(0x41, 0x54, 0x4C, 0x45, 0x44, 0x30, 0x3D, 0x31, 0x0D, 0x0A)
+            //                              A      T     L     E     D     0     =     1    \r    \n
+            limparSerial()
+            port.write(ligaLed, WRITE_WAIT_MILLIS)
+            receberDadosSerial()
+        }
+
+        botaoDesligarLed.setOnClickListener{
+            val desligaLed = byteArrayOf(0x41, 0x54, 0x4C, 0x45, 0x44, 0x30, 0x3D, 0x30, 0x0D, 0x0A)
+            //                              A      T     L     E     D     0     =     0    \r    \n
+
+            limparSerial()
+            port.write(desligaLed, WRITE_WAIT_MILLIS)
+            receberDadosSerial()
+        }
+
+        botaoColetarDadosCalibrados.setOnClickListener{
+            val coletarDadosCalibrados = byteArrayOf(0x41, 0x54, 0x43, 0x44, 0x41, 0x54, 0x41, 0x0D, 0x0A)
+            //                                        A      T     C     D     A     T     A    \r    \n
+
+            //limparSerial()
+            port.write(coletarDadosCalibrados, WRITE_WAIT_MILLIS)
+            receberDadosSerial()
+        }
+
+        botaoColetarDados.setOnClickListener{
+            val coletarDados = byteArrayOf(0x41, 0x54, 0x44, 0x41, 0x54, 0x41, 0x0D, 0x0A)
+            //                               A      T    D     A     T     A    \r    \n
+
+            limparSerial()
+            port.write(coletarDados, WRITE_WAIT_MILLIS)
+            receberDadosSerial()
+        }
+
+    }
+
+    private fun receberDadosSerial(){
+
+        var textoRetornoSerial = binding.textViewRetornoSerial
+        var len: Int
+        var buffer = ByteArray(1024)
+        var finalizador: Boolean = false
+        var retorno: String
+        var qtdDados: Int = 0
+        var dadosRecebidos: List<String>
+        var dados: List<Float>
+
+
+        while(!finalizador)
+        {
+            len = port.read(buffer, READ_WAIT_MILLIS);
+            retorno = String(buffer)
+
+            dadosRecebidos = retorno.split("OK")
+            qtdDados = dadosRecebidos.size
+
+            while (qtdDados <= 1)
+            {
+                len = port.read(buffer, READ_WAIT_MILLIS);
+                retorno = String(buffer)
+
+                dadosRecebidos = retorno.split("OK")
+                qtdDados = dadosRecebidos.size
+            }
+
+            for (c in 0 until qtdDados)
+            {
+                val toast = Toast.makeText(applicationContext, qtdDados.toString(), Toast.LENGTH_SHORT).show()
+            }
+
+            textoRetornoSerial.text = retorno
+
+            finalizador = true
+        }
+
+
+    }
+
+    private fun limparSerial(){
+        var len: Int
+        var buffer = ByteArray(1024)
+        len = port.read(buffer, READ_WAIT_MILLIS)
+        val toast = Toast.makeText(applicationContext, String(buffer), Toast.LENGTH_SHORT).show()
     }
 }
+
